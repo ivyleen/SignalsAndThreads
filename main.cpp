12672da178112d61d8c00b2dc07fed3a228f3ddd
Ivy Ivanova <ivaivanova091@gmail.com>
2020-05-23 16:12:30 +0100

    [thread] creating separate thread

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <csignal>
 #include <unistd.h>
+#include <pthread.h>
+
 
 void signalHandler (int sigNum)
 {
@@ -9,15 +11,37 @@ void signalHandler (int sigNum)
 }
 
 
-int main()
+void* sleeping( void* arg)
 {
-	std::signal(SIGINT, signalHandler);
-
 	while (1)
 	{
 		std::cout << "Sleeping ...\n";
 		sleep(1);
 	}
+
+	return NULL;
+}
+
+
+void awake()
+{
+	while (1)
+	{
+		std::cout << "I'm awake ...\n";
+		sleep(2);
+	}
+}
+
+
+int main()
+{
+	std::signal(SIGINT, signalHandler);
+
+
+	pthread_t newthread;
+	pthread_create(&newthread, NULL, sleeping, NULL);
+
+	awake();
 
 	return 0;			
 }
