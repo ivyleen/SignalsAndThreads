0ef8b45424b5d671370aaf0bfeb7c4fc28e4f48b
Ivy Ivanova <ivaivanova091@gmail.com>
2020-05-23 17:05:54 +0100

    [thread][bug fix] we need to wait for every thread to be finished before we terminate the program

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <csignal>
 #include <unistd.h>
 #include <pthread.h>
+#include <array>
 
+const int NUMBER_OF_THREADS = 2;
 
 void signalHandler (int sigNum)
 {
@@ -13,22 +15,34 @@ void signalHandler (int sigNum)
 
 void* sleeping( void* arg)
 {
-	while (1)
+	for (int i = 0; i < 10; ++i)
 	{
-		std::cout << "Sleeping ...\n";
+		std::cout << "Sleeping ... " << i << "\n";
 		sleep(1);
 	}
 
-	return NULL;
+	return nullptr;
+}
+
+
+void* wakingUp(void* arg)
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << "Waking up ... " << i << "\n";
+		sleep(2);
+	}
+
+	return nullptr;	
 }
 
 
 void awake()
 {
-	while (1)
+	for (int i = 0; i < 5; ++i)
 	{
-		std::cout << "I'm awake ...\n";
-		sleep(2);
+		std::cout << "I'm awake ... " << i << "\n";
+		sleep(3);
 	}
 }
 
@@ -38,10 +52,21 @@ int main()
 	std::signal(SIGINT, signalHandler);
 
 
-	pthread_t newthread;
-	pthread_create(&newthread, NULL, sleeping, NULL);
+	std::array<pthread_t, NUMBER_OF_THREADS> threads;
+	std::cout << "Creating thread number: 2.\n";
+	pthread_create(&threads.at(0), nullptr, wakingUp, nullptr );
+
+	std::cout << "Creating thread number: 1.\n";
+	pthread_create(&threads.at(1), nullptr, sleeping, nullptr );
+	
 
 	awake();
+
+	// wait for everythread to be finished before end of program
+	for (int i = 0; i < threads.size(); ++i)
+	{
+		pthread_join(threads.at(i), nullptr);
+	}
 
 	return 0;			
 }
