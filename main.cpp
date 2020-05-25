c8fcf800fb545ec964bcd92ed1ad8464b84920aa
Ivy Ivanova <ivaivanova091@gmail.com>
2020-05-25 20:23:44 +0100

    [thread] using std::thread to make a thread

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <csignal>
 #include <unistd.h>
-#include <pthread.h>
 #include <array>
+#include <thread>
+
 
 const int NUMBER_OF_THREADS = 2;
+static bool s_Sleeping = true;
+
 
 void signalHandler (int sigNum)
 {
@@ -14,33 +17,29 @@ void signalHandler (int sigNum)
 }
 
 
-void* sleeping( void* arg)
+void sleeping()
 {
-	int* iptr = new int(0);
-	while ( *iptr != 10)
+	using namespace std::literals::chrono_literals;
+
+	while ( s_Sleeping )
 	{
-		sleep(1);
-		std::cout << "Thread №2: Sleeping ... " << *iptr << "\n";
-		(*iptr)++;
+		std::cout << "Sleeping ... on thread: " << std::this_thread::get_id() << std::endl;
+		std::this_thread::sleep_for(1s);
 	}
 
-	return iptr;
 }
 
 
-void* wakingUp(void* arg)
+void wakingUp()
 {
-	int* iptr = static_cast<int*>(arg);
+	using namespace std::literals::chrono_literals;
 
-	while (*iptr != 10)
+	while ( s_Sleeping )
 	{
-		sleep(2);
-		std::cout << "Thread №1: Waking up ... " << *iptr << "\n";
-		(*iptr)++;
+		std::cout << "Sleeping ... on thread: " << std::this_thread::get_id() << std::endl;
+		std::this_thread::sleep_for(2s);
 	}
-
-	*iptr = 0;
-	return nullptr;	
+		
 }
 
 
@@ -61,26 +60,16 @@ int main()
 	// it will not work because the KILL signal is a command
 	//std::signal(SIGKILL, signalHandler);
 
-	void* i = 0;	
+	std::thread sleepingThread( sleeping );
+//	std::thread wakingUpThread( wakingUp );	
 
-	// threads creation
-	std::array<pthread_t, NUMBER_OF_THREADS> threads;
-	std::cout << "Creating thread number: 1.\n";
-	pthread_create(&threads.at(0), nullptr, wakingUp, &i );
-
-	std::cout << "Creating thread number: 2.\n";
-	pthread_create(&threads.at(1), nullptr, sleeping, nullptr );
-	
 	// normal call of a function
 	awake();
 
-	// wait for everythread to be finished before end of program
-	for (int j = 0; j < threads.size(); ++j)
-	{
-		pthread_join(threads.at(j), &i);
-	}
-
-	std::cout << "i should be 10 now: " << *(static_cast<int*>(i)) << "\n";
+	//after main thread is over printing I'm awake stop the sleeping thread
+	s_Sleeping = false;
+	sleepingThread.join();
+	
 	// terminate the program
 	return 0;			
 }
